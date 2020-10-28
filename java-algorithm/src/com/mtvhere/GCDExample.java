dbefb3f4ed19e6e997f28bf4568a615c8cd0ba6f
Murali <muralitharan.varatharajan@ihg.com>
2020-10-28 17:47:56 -0400

    added gcd sample

---
+++ java-algorithm/src/com/mtvhere/GCDExample.java
@@ -0,0 +1,19 @@
+package com.mtvhere;
+
+public class GCDExample {
+
+    public static void main(final String[] arg) {
+
+        final int num1 = 55;
+        final int num2 = 121;
+        int gcd = 1;
+
+        for (int i = 1; i <= num1 && i <= num2; i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                gcd = i;
+            }
+        }
+        System.out.printf("GCD of %d and %d is: %d", num1, num2, gcd);
+    }
+
+}
